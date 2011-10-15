2a75223eab40357bb5ad6d348ef8a37b280aeb35
rouchel <rouchel@maxwit.com>
2011-10-15 22:11:15 +0800

    update run.sh
    Signed-off-by: rouchel <rouchel@maxwit.com>

--- build/simulator/omap3530/run.sh
+++ build/simulator/omap3530/run.sh
@@ -5,11 +5,8 @@ MACHINE="beagle"
 #MACHINE="mw71"
 NANDIMG="omap3-nand.img"
 
-rm -vf $NANDIMG
-./bb_nandflash.sh g-bios-th.bin ${NANDIMG}
-./bb_nandflash.sh g-bios-bh.bin ${NANDIMG}
-./bb_nandflash.sh zImage ${NANDIMG}
-./bb_nandflash.sh rootfs ${NANDIMG}
+./create_nandflash.py -i g-bios-th.bin -o ${NANDIMG}
+./create_nandflash.py -i g-bios-bh.bin -o ${NANDIMG}
 
 #sudo ${QEMUCMMOND} -M ${MACHINE} -mtdblock ${NANDIMG} -serial stdio -net nic -net tap -sd sd.img
 ${QEMUCMMOND} -M ${MACHINE} -mtdblock ${NANDIMG} -serial stdio
